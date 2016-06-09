12badcb1a0ffc2d09054beeadc4571fc82ba8d2f
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-06-09 21:37:54 +0200

    Add a test case with linear inheritence and methods.

--- pyclsflatten/__init__.py
+++ pyclsflatten/__init__.py
@@ -1,6 +1,25 @@
+from collections import OrderedDict
 import inspect
+import itertools
+import operator
+
+
+def _get_defined_methods(cls):
+    '''
+    Get all the methods that are defined on *that* class, ignoring parents.
+    '''
+    ret = []
+    for name, val in cls.__dict__.items():
+        if inspect.isfunction(val):
+            lines, lnum = inspect.getsourcelines(val)
+            ret.append((name, inspect.getsource(val)))
+    return ret
 
 
 def flatten(cls):
-    body = ''.join(inspect.getsourcelines(cls)[0][1:])
+    methods = OrderedDict()
+    for subcls in cls.__mro__[-2::-1]:
+        methods.update(_get_defined_methods(subcls))
+
+    body = '\n'.join(methods.values()) or '    pass\n'
     return 'class {}(object):\n{}'.format(cls.__name__, body)
--- tests/test_linear_inheritence.py
+++ tests/test_linear_inheritence.py
@@ -11,10 +11,31 @@ class SimpleExtension(SimpleBase):
     pass
 
 
+class ComplexBase(object):
+    def methodA(self):
+        pass
+
+
+class ComplexExtension(ComplexBase):
+    def methodB(self):
+        pass
+
+
 class LinearExampleTests(TestCase):
     def test_simple(self):
         output = flatten(SimpleExtension)
         self.assertEqual(output, """\
 class SimpleExtension(object):
     pass
+""")
+
+    def test_complex(self):
+        output = flatten(ComplexExtension)
+        self.assertEqual(output, """\
+class ComplexExtension(object):
+    def methodA(self):
+        pass
+
+    def methodB(self):
+        pass
 """)
